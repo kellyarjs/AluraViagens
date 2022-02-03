f0d521e475b750c5632ed301b5c63d63986b7e32
kellyarjs <kellyarjs@gmail.com>
2022-02-03 10:36:21 -0300

    Initial Commit

---
+++ AluraViagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AluraViagens
+//
+//  Created by Kelly Silva Araujo on 03/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
